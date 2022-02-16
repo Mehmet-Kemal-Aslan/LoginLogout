2661f45f3b531948fa911129133a65203ab161ba
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2022-02-16 06:42:43 +0300

    first commit

---
+++ Models/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginLogout.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace LoginLogout.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }  
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
