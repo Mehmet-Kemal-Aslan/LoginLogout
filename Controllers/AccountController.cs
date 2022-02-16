030426f6c2f8010ed16cbcaf74705bc5bc2c419c
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2022-02-16 23:17:09 +0300

    ikinci gönderme

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginLogout.Models;
+
+
+namespace LoginLogout.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        private readonly DataContext _context;
+
+        public AccountController(ILogger<AccountController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(Users User)
+        {
+            if (User.Id == 0)
+            {
+                await _context.AddAsync(User);
+            }
+            else
+            {
+                _context.Update(User);
+            }
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
